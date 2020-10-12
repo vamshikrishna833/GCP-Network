d76d94e699bf89ec2e5021f29d2913717a0a8a52
vamshikrishna833 <45107460+vamshikrishna833@users.noreply.github.com>
2020-10-12 11:01:44 -0600

    Update variable.tf

--- modules/network-peering/variable.tf
+++ modules/network-peering/variable.tf
@@ -1,15 +1,20 @@
 variable "vpc1_project_id" {
   description = "Enter the vpc1 project id"
+  type        = string
 }
 variable "vpc1_network_name" {
   description = "Enter the vpc1 project network name"
+  type        = string
 }
 variable "vpc2_project_id" {
   description = "Enter the vpc2ination project id"
+  type        = string
 }
 variable "vpc2_network_name" {
   description = "Enter the vpc2ination project network name"
+  type        = string
 }
 variable "depends_on_connection" {
   description = "To check if VPC peering is free"
+  type        = string
 }
